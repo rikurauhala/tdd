eb5e4f230f701ac668be6e976d5308bc6cac2e2f
Riku Rauhala <riku@rauhala.io>
2024-01-04 11:21:21 +0200

    Convert date in the calculateReduction function call

--- exercises/small-steps/src/prices.mjs
+++ exercises/small-steps/src/prices.mjs
@@ -59,7 +59,7 @@ function createApp(database) {
   }
 
   function calculateCostForDayTicket(age, date, baseCost) {
-    let reduction = calculateReduction(date);
+    let reduction = calculateReduction(convert(date));
     if (age === undefined) {
       return Math.ceil(baseCost * (1 - reduction / 100));
     }
@@ -77,7 +77,7 @@ function createApp(database) {
 
   function calculateReduction(date) {
     let reduction = 0;
-    if (date && isMonday(convert(date)) && !isHoliday(convert(date))) {
+    if (date && isMonday(date) && !isHoliday(date)) {
       reduction = 35;
     }
     return reduction;
