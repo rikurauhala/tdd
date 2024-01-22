459340510c0a25af73b9183dedae5bf096724675
Riku Rauhala <riku@rauhala.io>
2024-01-22 20:26:45 +0200

    Prevent dropping another block while one is already falling

--- exercises/tetris/src/Board.mjs
+++ exercises/tetris/src/Board.mjs
@@ -3,6 +3,7 @@ export class Board {
     this.width = width;
     this.height = height;
     this.board = this.resetBoard(width, height);
+    this.falling = false;
     this.previousPosition = null;
     this.previousBlock = null;
   }
@@ -14,7 +15,11 @@ export class Board {
   }
 
   drop(block) {
+    if (this.falling) {
+      throw new Error("already falling");
+    }
     this.board[0][1] = block;
+    this.falling = true;
     this.previousPosition = [0, 1];
     this.previousBlock = block;
   }
