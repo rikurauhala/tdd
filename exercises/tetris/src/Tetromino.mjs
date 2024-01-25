a76dc33d6f9146510d4ab5826ec8185c0f501615
Riku Rauhala <riku@rauhala.io>
2024-01-25 20:39:01 +0200

    Implement rotateLeft

--- exercises/tetris/src/Tetromino.mjs
+++ exercises/tetris/src/Tetromino.mjs
@@ -15,6 +15,10 @@ export class Tetromino {
     return this.shape.rotateRight();
   }
 
+  rotateLeft() {
+    return this.shape.rotateLeft();
+  }
+
   toString() {
     return this.shape.toString();
   }
