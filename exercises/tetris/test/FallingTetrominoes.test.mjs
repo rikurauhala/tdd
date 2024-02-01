a04515db311d1a8f9771c9e6e4f62abcb0e88214
Riku Rauhala <riku@rauhala.io>
2024-02-01 12:13:55 +0200

    Write more tests for falling I and O shapes

--- exercises/tetris/test/FallingTetrominoes.test.mjs
+++ exercises/tetris/test/FallingTetrominoes.test.mjs
@@ -9,7 +9,7 @@ function fallToBottom(board) {
   }
 }
 
-describe("Falling tetrominoes", () => {
+describe("Falling T shape tetrominoes", () => {
   let board;
   beforeEach(() => {
     board = new Board(10, 6);
@@ -55,6 +55,106 @@ describe("Falling tetrominoes", () => {
        ...TTT....
        ....T.....
        ...TTT....`
+    );
+  });
+});
+
+describe("Falling O shape tetrominoes", () => {
+  let board;
+  beforeEach(() => {
+    board = new Board(10, 6);
+  });
+
+  test("start from the top middle", () => {
+    board.drop(Tetromino.O_SHAPE);
+
+    expect(board.toString()).to.equalShape(
+      `....OO....
+       ....OO....
+       ..........
+       ..........
+       ..........
+       ..........`
+    );
+  });
+
+  test("stop when they hit the bottom", () => {
+    board.drop(Tetromino.O_SHAPE);
+    fallToBottom(board);
+
+    expect(board.toString()).to.equalShape(
+      `..........
+       ..........
+       ..........
+       ..........
+       ....OO....
+       ....OO....`
+    );
+  });
+
+  test("stop when they land on another block", () => {
+    board.drop(Tetromino.T_SHAPE);
+    fallToBottom(board);
+    board.drop(Tetromino.O_SHAPE);
+    fallToBottom(board);
+
+    expect(board.toString()).to.equalShape(
+      `..........
+       ..........
+       ....OO....
+       ....OO....
+       ....T.....
+       ...TTT....`
+    );
+  });
+});
+
+describe("Falling I shape tetrominoes", () => {
+  let board;
+  beforeEach(() => {
+    board = new Board(10, 6);
+  });
+
+  test("start from the top middle", () => {
+    board.drop(Tetromino.I_SHAPE);
+
+    expect(board.toString()).to.equalShape(
+      `..IIII....
+       ..........
+       ..........
+       ..........
+       ..........
+       ..........`
+    );
+  });
+
+  test("stop when they hit the bottom", () => {
+    board.drop(Tetromino.I_SHAPE);
+    fallToBottom(board);
+
+    expect(board.toString()).to.equalShape(
+      `..........
+       ..........
+       ..........
+       ..........
+       ..........
+       ..IIII....`
+    );
+  });
+
+  test("stop when they land on another block", () => {
+    board.drop(Tetromino.I_SHAPE);
+    fallToBottom(board);
+    board.drop(Tetromino.T_SHAPE);
+    fallToBottom(board);
+
+    expect(board.toString()).to.equalShape(
+      `..........
+       ..........
+       ..........
+       ....T.....
+       ...TTT....
+       ..IIII....`
     );
   });
 });
