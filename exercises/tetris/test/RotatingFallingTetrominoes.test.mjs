7834effce53c8ad1372cff317396eb04a1e2cf55
Riku Rauhala <riku@rauhala.io>
2024-02-02 18:43:05 +0200

    Begin testing rotating falling tetrominoes

---
+++ exercises/tetris/test/RotatingFallingTetrominoes.test.mjs
@@ -0,0 +1,26 @@
+import { beforeEach, describe, test } from "vitest";
+import { expect } from "chai";
+import { Board } from "../src/Board.mjs";
+import { Tetromino } from "../src/Tetromino.mjs";
+
+describe("Falling T shape tetrominoes", () => {
+  let board;
+  beforeEach(() => {
+    board = new Board(10, 6);
+    board.drop(Tetromino.T_SHAPE);
+  });
+
+  describe("can be rotated left", () => {
+    test("once", () => {
+      board.rotateLeft();
+      expect(board.toString()).to.equalShape(
+        `....T.....
+         ...TT.....
+         ....T.....
+         ..........
+         ..........
+         ..........`
+      );
+    });
+  });
+});
