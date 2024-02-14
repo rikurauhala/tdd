e6c2b21292ef7b7179bc6af761532c99ee995440
Riku Rauhala <riku@rauhala.io>
2024-02-14 22:55:00 +0200

    Write tests for the S shape

--- exercises/tetris/test/RotatingTetrominoes.test.mjs
+++ exercises/tetris/test/RotatingTetrominoes.test.mjs
@@ -140,6 +140,38 @@ describe("The O shape", () => {
   });
 });
 
+describe("The S shape", () => {
+  const shape = Tetromino.S_SHAPE;
+
+  test("initial orientation", () => {
+    expect(shape.toString()).to.equalShape(
+      `.SS
+       SS.
+       ...`
+    );
+  });
+
+  test("can be rotated right/clockwise", () => {
+    expect(shape.rotateRight().toString()).to.equalShape(
+      `S..
+       SS.
+       .S.`
+    );
+  });
+
+  test("can be rotated left/counter-clockwise", () => {
+    expect(shape.rotateLeft().toString()).to.equalShape(
+      `S..
+       SS.
+       .S.`
+    );
+  });
+
+  test("has 2 distinct orientations", () => {
+    expect(distinctOrientations(shape).size).to.equal(2);
+  });
+});
+
 describe("The T shape", () => {
   const shape = Tetromino.T_SHAPE;
 
