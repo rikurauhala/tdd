07671dd85ff05057354dc1705ec8a4df21dc5ec8
Riku Rauhala <riku@rauhala.io>
2024-02-15 20:36:16 +0200

    Write tests for clearing one line

---
+++ exercises/tetris/test/ClearingLines.test.mjs
@@ -0,0 +1,40 @@
+import { beforeEach, describe, test } from "vitest";
+import { expect } from "chai";
+import { Board } from "../src/Board.mjs";
+import { Tetromino } from "../src/Tetromino.mjs";
+
+describe("Clearing lines", () => {
+  let board;
+  beforeEach(() => {
+    board = new Board(10, 6);
+    board.drop(Tetromino.I_SHAPE);
+    board.rotateRight();
+    for (let col = 0; col < 6; col++) {
+      board.moveRight();
+    }
+  });
+
+  test("works for one line", () => {
+    for (let col = 0; col < 9; col++) {
+      board.board[5][col] = "X";
+    }
+    board.tick();
+    expect(board.toString()).to.equalShape(
+      `..........
+       .........I
+       .........I
+       .........I
+       .........I
+       XXXXXXXXX.`
+    );
+    board.tick();
+    expect(board.toString()).to.equalShape(
+      `..........
+       ..........
+       ..........
+       .........I
+       .........I
+       .........I`
+    );
+  });
+});
