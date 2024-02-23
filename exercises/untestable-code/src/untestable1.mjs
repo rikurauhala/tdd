ae422a92f914018b8097417665ef6bff37e2df54
Riku Rauhala <riku@rauhala.io>
2024-02-23 20:02:45 +0200

    Refactor untestable1

--- exercises/untestable-code/src/untestable1.mjs
+++ exercises/untestable-code/src/untestable1.mjs
@@ -1,11 +1,9 @@
 const millisPerDay = 24 * 60 * 60 * 1000;
 
-export function daysUntilChristmas() {
-  const now = new Date();
-  const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-  const christmasDay = new Date(now.getFullYear(), 12 - 1, 25);
+export function daysUntilChristmas(today = new Date()) {
+  const christmasDay = new Date(today.getFullYear(), 12 - 1, 25);
   if (today.getTime() > christmasDay.getTime()) {
-    christmasDay.setFullYear(new Date().getFullYear() + 1);
+    christmasDay.setFullYear(today.getFullYear() + 1);
   }
   const diffMillis = christmasDay.getTime() - today.getTime();
   return Math.floor(diffMillis / millisPerDay);
