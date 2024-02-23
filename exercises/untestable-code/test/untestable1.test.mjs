18ef3a3af3c499ad299c04de58213878ac8a0268
Riku Rauhala <riku@rauhala.io>
2024-02-23 20:02:37 +0200

    Write tests for untestable1

--- exercises/untestable-code/test/untestable1.test.mjs
+++ exercises/untestable-code/test/untestable1.test.mjs
@@ -3,8 +3,20 @@ import { expect } from "chai";
 import { daysUntilChristmas } from "../src/untestable1.mjs";
 
 describe("Untestable 1: days until Christmas", () => {
-  test("todo", () => {
-    // TODO: write proper tests
-    expect(daysUntilChristmas()).to.be.a("number");
+  test("returns a number", () => {
+    const days = daysUntilChristmas();
+    expect(days).to.be.a("number");
+  });
+
+  test("calculates days until Christmas for the current year", () => {
+    const date = new Date(2023, 11, 1);
+    const days = daysUntilChristmas(date);
+    expect(days).to.equal(24);
+  });
+
+  test("calculates days until Christmas for the next year", () => {
+    const date = new Date(2023, 11, 26);
+    const days = daysUntilChristmas(date);
+    expect(days).to.equal(365);
   });
 });
