d98117715cb51715ed90f805307747223ebf7470
Riku Rauhala <riku@rauhala.io>
2024-02-23 23:36:59 +0200

    Refactor untestable2

--- exercises/untestable-code/src/untestable2.mjs
+++ exercises/untestable-code/src/untestable2.mjs
@@ -4,14 +4,12 @@ function diceRoll() {
   return Math.floor(Math.random() * (max + 1 - min) + min);
 }
 
-export function diceHandValue() {
-  const die1 = diceRoll();
-  const die2 = diceRoll();
+export function diceHandValue(roll1 = diceRoll, roll2 = diceRoll) {
+  const die1 = roll1();
+  const die2 = roll2();
   if (die1 === die2) {
-    // one pair
     return 100 + die1;
   } else {
-    // high die
     return Math.max(die1, die2);
   }
 }
