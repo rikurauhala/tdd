5437dd4144c869de1e8f490f70ab6593c69a63dc
Riku Rauhala <riku@rauhala.io>
2024-02-24 15:06:13 +0200

    Write tests for untestable4

--- exercises/untestable-code/test/untestable4.test.mjs
+++ exercises/untestable-code/test/untestable4.test.mjs
@@ -1,17 +1,31 @@
 import { afterEach, beforeEach, describe, test } from "vitest";
-import { PasswordService, PostgresUserDao } from "../src/untestable4.mjs";
+import { expect } from "chai";
+import { PasswordService, PostgresUserDao } from "../src/testable4.mjs";
 
 describe("Untestable 4: enterprise application", () => {
   let service;
+  let postgresUserDao;
   beforeEach(() => {
     service = new PasswordService();
+    postgresUserDao = PostgresUserDao.getInstance();
   });
 
   afterEach(() => {
-    PostgresUserDao.getInstance().close();
+    postgresUserDao.close();
   });
 
-  test("todo", async () => {
-    // TODO: write proper tests for both PasswordService and PostgresUserDao
+  test("database connection options are correct", async () => {
+    expect(postgresUserDao.getDb().options.user).to.equal("untestable");
+    expect(postgresUserDao.getDb().options.host).to.equal("localhost");
+    expect(postgresUserDao.getDb().options.database).to.equal("untestable");
+    expect(postgresUserDao.getDb().options.password).to.equal("secret");
+    expect(postgresUserDao.getDb().options.port).to.equal(5432);
+  });
+
+  test("user is saved and retrieved", async () => {
+    const user = { userId: "user", passwordHash: "pass" };
+    await postgresUserDao.save(user);
+    const retrieved = await postgresUserDao.getById(user.userId);
+    expect(retrieved).to.deep.equal(user);
   });
 });
